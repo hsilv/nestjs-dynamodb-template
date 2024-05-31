0b056484edaa3cddb67f7b3da4d5c2cdbefc1a18
hsilv <hm.sebastiansilva@gmail.com>
2024-05-31 03:06:45 -0600

    feat: minifier

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,12 +1,13 @@
 import { configure as serverlessExpress } from '@vendia/serverless-express';
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 let cachedServer;
 
 export const handler = async (event, context) => {
     if (!cachedServer) {
-        const nestApp = await NestFactory.create(AppModule);
+        const nestApp = await NestFactory.create(AppModule, new ExpressAdapter());
         await nestApp.init();
         cachedServer = serverlessExpress({
             app: nestApp.getHttpAdapter().getInstance(),
